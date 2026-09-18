dc71d03170c5610b4bd48d9941ae71202c4b3352
XeldarAlz <xeldaralz@hotmail.com>
2026-09-18 12:18:48 +0300

    chore(release): bump the version to 1.0.3.3

--- src/Aetherphone/Core/Changelog/ChangelogData.cs
+++ src/Aetherphone/Core/Changelog/ChangelogData.cs
@@ -6,7 +6,7 @@ internal static class ChangelogData
 {
     public static readonly IReadOnlyList<ChangelogEntry> Entries = new[]
     {
-        new ChangelogEntry("1.0.3.3", "2026-09-16", new ChangelogSection[]
+        new ChangelogEntry("1.0.3.3", "2026-09-18", new ChangelogSection[]
         {
             new(L.Changelog.SectionSocial, L.Changelog.Release1033Social),
             new(L.Changelog.SectionChirperAethergramVelvet, L.Changelog.Release1033ChirperAethergramVelvet),
